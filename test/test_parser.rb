83bfd55741e0ba458a40b7db68604c55b249e025
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2020-01-22 15:37:04 +0900

    profile data

--- test/test_parser.rb
+++ test/test_parser.rb
@@ -5,4 +5,13 @@ class TestMachines < Minitest::Test
   def test_itac
     
   end
+
+  def test_starpu_profile
+    parser = Parser::StarpuProfile.new("test/artifacts/profile_file_DENSE_*.starpu_profile")
+
+    puts parser.total_time
+    puts parser.total_exec_time
+    puts parser.total_sleep_time
+    puts parser.total_overhead_time
+  end
 end
